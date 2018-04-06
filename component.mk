731c9bd7a847ff039fd082fc36d70f5831e6bd17
Ashley Towns <mail@ashleytowns.id.au>
2018-04-07 02:46:06 +1000

    update component makefile

--- component.mk
+++ component.mk
@@ -1,5 +1,5 @@
 CFLAGS+=-DFORCE_ALIGNMENT -DIDRIS_TARGET_OS=\"none\" -DIDRIS_TARGET_TRIPLE=\"xtensa-esp32-elf\" -DESP32
 COMPONENT_ADD_INCLUDEDIRS=rts
-COMPONENT_OBJS=rts/idris_gc.o rts/idris_rts.o rts/idris_bitstring.o rts/idris_gmp.o rts/idris_heap.o rts/mini-gmp.o rts/idris_stdfgn.o
+COMPONENT_OBJS=rts/idris_gc.o rts/idris_rts.o rts/idris_bitstring.o rts/idris_gmp.o rts/idris_heap.o rts/mini-gmp.o rts/idris_stdfgn.o rts/idris_net.o rts/idris_buffer.o rts/idris_utf8.o rts/idris_stats.o
 COMPONENT_SRCDIRS=rts
 
